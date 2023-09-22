9fe33b73b43b8e67dc9227279856a6cf739345a5
Altamash <altamashshaikh_ce_2020@ltce.in>
2023-09-22 23:36:42 +0530

    docs changes again

--- docs/static/chunks/app/page-a4b1bf10b506a83c.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4969:function(e,t,r){Promise.resolve().then(r.bind(r,593))},593:function(e,t,r){"use strict";r.r(t);var n=r(7437),s=r(2265);t.default=()=>{let[e,t]=(0,s.useState)(""),[r,o]=(0,s.useState)(80);return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsxs)("h1",{children:["My marks are ",r]}),(0,n.jsxs)("form",{action:"",onSubmit:r=>{r.preventDefault(),o(e),t("")},children:[(0,n.jsx)("input",{type:"text",name:"marks",id:"marks",value:e,onChange:e=>{e.preventDefault(),t(e.target.value)}}),(0,n.jsx)("button",{type:"submit",children:"Change Marks"})]})]})}},622:function(e,t,r){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=r(2265),s=Symbol.for("react.element"),o=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,a=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function f(e,t,r){var n,o={},f=null,l=null;for(n in void 0!==r&&(f=""+r),void 0!==t.key&&(f=""+t.key),void 0!==t.ref&&(l=t.ref),t)u.call(t,n)&&!i.hasOwnProperty(n)&&(o[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===o[n]&&(o[n]=t[n]);return{$$typeof:s,type:e,key:f,ref:l,props:o,_owner:a.current}}t.Fragment=o,t.jsx=f,t.jsxs=f},7437:function(e,t,r){"use strict";e.exports=r(622)}},function(e){e.O(0,[971,864,744],function(){return e(e.s=4969)}),_N_E=e.O()}]);
